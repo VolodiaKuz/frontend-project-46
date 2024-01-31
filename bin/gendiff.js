22daecc466f939db35aa2a3a3754bbc49a5e70e5
Volodia K <wtraktorman94@mail.ru>
2024-01-31 14:03:06 +0600

    write the 4th step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,16 +1,16 @@
 #!/usr/bin/env node
-import gendiff from '../src/index.js';
+import genDiff from '../src/index.js';
 import { program } from 'commander';
 
 program
   .name('gendiff')
-  .description('  Compares two configuration files and shows a difference.')
+  .description('  Compares two configuration files and shows the difference.')
   .version('1.0.0')
   .argument('<filepath1>', 'path to first file')
   .argument('<filepath2>', 'path to second file')
   .option('-f, --format <type>', 'output format')
   .action((filepath1, filepath2) => {
-    const result = gendiff(filepath1, filepath2);
+    const result = genDiff(filepath1, filepath2);
     console.log(result);
   });
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
 #!/usr/bin/env node
-//  const path = require('path');
 import { pathResolver, fileReader } from './utils.js';
 
-const gendiff = (filepath1, filepath2) => {
+const genDiff = (filepath1, filepath2) => {
   const file1 = fileReader(pathResolver(filepath1));
   const file2 = fileReader(pathResolver(filepath2));
   const obj1 = JSON.parse(file1);
   const obj2 = JSON.parse(file2);
-  console.log(obj1);
-  console.log(obj2);
-  return obj1;
+  const sortedKeys1 = Object.keys(obj1).sort();
+  const sortedKeys2 = Object.keys(obj2).sort();
+  
+  let result = '';
+  result += '{';
+  for (const key of sortedKeys1) {
+    if (Object.hasOwn(obj2, key) && obj1[key] === obj2[key]) {
+      result += `\n    ${key}: ${obj1[key]}`;
+    } else if (Object.hasOwn(obj2, key) && obj1[key] !== obj2[key]) {
+      result += `\n  - ${key}: ${obj1[key]}`;
+      result += `\n  + ${key}: ${obj2[key]}`;
+    } else if (!Object.hasOwn(obj2, key)) {
+      result += `\n  - ${key}: ${obj1[key]}`;
+    }
+  }
+  for (const key of sortedKeys2) {
+    if (!Object.hasOwn(obj1, key)) {
+      result += `\n  + ${key}: ${obj2[key]}`;
+    }
+  }
+  result += '\n}';
+  return result;
 };
 
-export default gendiff;
+export default genDiff;
