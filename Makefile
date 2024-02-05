f762867749ecf110965daa1fe3f78926673052fe
Volodia K <wtraktorman94@mail.ru>
2024-02-05 23:48:06 +0600

    add tests for gendiff function

--- Makefile
+++ Makefile
@@ -9,4 +9,7 @@ publish:
 
 lint:
 	npx eslint .
+
+test:
+	NODE_OPTIONS=--experimental-vm-modules npx jest
 				
---
+++ __tests__/index.test.js
@@ -0,0 +1,8 @@
+import genDiff from '../src/index.js';
+
+const flatJson = '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}';
+
+test('genDiff', () => {
+  expect(genDiff('./__fixtures__/file1.json', './__fixtures__/file2.json')).toEqual(flatJson);
+  // expect(genDiff('')).toEqual('');
+});
