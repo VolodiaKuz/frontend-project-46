9c99b1767749413a361947159ac5a944b9aa0d83
Volodia K <wtraktorman94@mail.ru>
2024-03-05 00:58:59 +0500

    edit formatter stylish 1 attempt

--- formatters/formatter.stylish.js
+++ formatters/formatter.stylish.js
@@ -1,45 +1,115 @@
 import _ from 'lodash';
 import { printObject } from '../src/utils.js';
 
-const getStylishFormat = (diffArray, replacer = ' ') => {
-  const result = '';
-  let str = '{\n';
+// const getStylishFormat = (diffArray, replacer = ' ') => {
+//   const result = '';
+//   let str = '{\n';
 
-  const iter = (node1, depth) => {
-    for (const obj of node1.values()) {
-      if (obj.state === 'similar') {
-        str += `${replacer.repeat(depth * 4 - 2)}  ${obj.keyName}: ${obj.keyValue}\n`;
-      }
-      if (obj.state === 'updated') {
-        if (_.isObject(obj.oldValue)) {
-          str += `${replacer.repeat(depth * 4 - 2)}- ${obj.keyName}: `;
-          str += `${printObject(obj.oldValue, depth * 4)}\n`;
-        } else str += `${replacer.repeat(depth * 4 - 2)}- ${obj.keyName}: ${obj.oldValue}\n`;
-        if (_.isObject(obj.newValue)) {
-          str += `${replacer.repeat(depth * 4 - 2)}+ ${obj.keyName}: `; // возможно здесь должен быть плюс
-          str += `${printObject(obj.newValue, depth * 4)}\n`;
-        } else str += `${replacer.repeat(depth * 4 - 2)}+ ${obj.keyName}: ${obj.newValue}\n`;
-      }
-      if (obj.state === 'removed') {
-        if (_.isObject(obj.keyValue)) {
-          str += `${replacer.repeat(depth * 4 - 2)}- ${obj.keyName}: `;
-          str += `${printObject(obj.keyValue, depth * 4)}\n`;
-        } else str += `${replacer.repeat(depth * 4 - 2)}- ${obj.keyName}: ${obj.keyValue}\n`;
-      }
-      if (obj.state === 'added') {
-        if (_.isObject(obj.keyValue)) {
-          str += `${replacer.repeat(depth * 4 - 2)}+ ${obj.keyName}: `;
-          str += `${printObject(obj.keyValue, depth * 4)}\n`;
-        } else str += `${replacer.repeat(depth * 4 - 2)}+ ${obj.keyName}: ${obj.keyValue}\n`;
-      }
-      if (obj.state === 'nested') {
-        str += `${replacer.repeat(depth * 4 - 2)}  ${obj.keyName}: {\n`;
-        iter(obj.keyValue, depth + 1);
-        str += `${replacer.repeat(depth * 4)}}\n`;
-      }
+//   const iter = (node1, depth) => {
+//     console.log(node1);
+//     for (const obj of node1.values()) {
+//       if (obj.state === 'similar') {
+//         str += `${replacer.repeat(depth * 4 - 2)}  ${obj.keyName}: ${obj.keyValue}\n`;
+//       }
+//       if (obj.state === 'updated') {
+//         if (_.isObject(obj.oldValue)) {
+//           str += `${replacer.repeat(depth * 4 - 2)}- ${obj.keyName}: `;
+//           str += `${printObject(obj.oldValue, depth * 4)}\n`;
+//         } else str += `${replacer.repeat(depth * 4 - 2)}- ${obj.keyName}: ${obj.oldValue}\n`;
+//         if (_.isObject(obj.newValue)) {
+//           str += `${replacer.repeat(depth * 4 - 2)}+ ${obj.keyName}: `; // возможно здесь должен быть плюс
+//           str += `${printObject(obj.newValue, depth * 4)}\n`;
+//         } else str += `${replacer.repeat(depth * 4 - 2)}+ ${obj.keyName}: ${obj.newValue}\n`;
+//       }
+//       if (obj.state === 'removed') {
+//         if (_.isObject(obj.keyValue)) {
+//           str += `${replacer.repeat(depth * 4 - 2)}- ${obj.keyName}: `;
+//           str += `${printObject(obj.keyValue, depth * 4)}\n`;
+//         } else str += `${replacer.repeat(depth * 4 - 2)}- ${obj.keyName}: ${obj.keyValue}\n`;
+//       }
+//       if (obj.state === 'added') {
+//         if (_.isObject(obj.keyValue)) {
+//           str += `${replacer.repeat(depth * 4 - 2)}+ ${obj.keyName}: `;
+//           str += `${printObject(obj.keyValue, depth * 4)}\n`;
+//         } else str += `${replacer.repeat(depth * 4 - 2)}+ ${obj.keyName}: ${obj.keyValue}\n`;
+//       }
+//       if (obj.state === 'nested') {
+//         str += `${replacer.repeat(depth * 4 - 2)}  ${obj.keyName}: {\n`;
+//         iter(obj.keyValue, depth + 1);
+//         str += `${replacer.repeat(depth * 4)}}\n`;
+//       }
+//     }
+//     return str;
+//   };
+//   return `${result}${iter(diffArray, 1)}}`;
+// };
+
+const getFormattedArray = (diff, depth = 1) => {
+  const replacer = ' ';
+  const result = diff.flatMap((obj) => {
+
+    if (obj.state === 'similar') {
+      const str = `${replacer.repeat(depth * 4 - 2)}  ${obj.keyName}: ${obj.keyValue}\n`;
+      return {
+        str,
+        depth,
+      };
     }
-    return str;
-  };
-  return `${result}${iter(diffArray, 1)}}`;
-};
+    if (obj.state === 'updated') {
+      let str = '';
+      if (_.isObject(obj.oldValue)) {
+        str += `${replacer.repeat(depth * 4 - 2)}- ${obj.keyName}: `;
+        str += `${printObject(obj.oldValue, depth * 4)}\n`;
+      } else str += `${replacer.repeat(depth * 4 - 2)}- ${obj.keyName}: ${obj.oldValue}\n`;
+      if (_.isObject(obj.newValue)) {
+        str += `${replacer.repeat(depth * 4 - 2)}+ ${obj.keyName}: `; // возможно здесь должен быть плюс
+        str += `${printObject(obj.newValue, depth * 4)}\n`;
+      } else str += `${replacer.repeat(depth * 4 - 2)}+ ${obj.keyName}: ${obj.newValue}\n`;
+      return {
+        str,
+        depth,
+      };
+    }
+    if (obj.state === 'removed') {
+      let str = '';
+      if (_.isObject(obj.keyValue)) {
+        str += `${replacer.repeat(depth * 4 - 2)}- ${obj.keyName}: `;
+        str += `${printObject(obj.keyValue, depth * 4)}\n`;
+      } else str += `${replacer.repeat(depth * 4 - 2)}- ${obj.keyName}: ${obj.keyValue}\n`;
+      return {
+        str,
+        depth,
+      };
+    }
+    if (obj.state === 'added') {
+      let str = '';
+      if (_.isObject(obj.keyValue)) {
+        str += `${replacer.repeat(depth * 4 - 2)}+ ${obj.keyName}: `;
+        str += `${printObject(obj.keyValue, depth * 4)}\n`;
+      } else str += `${replacer.repeat(depth * 4 - 2)}+ ${obj.keyName}: ${obj.keyValue}\n`;
+      return {
+        str,
+        depth,
+      };
+    }
+    if (obj.state === 'nested') {
+      let str = '';
+      str += `${replacer.repeat(depth * 4 - 2)}  ${obj.keyName}: {\n`;
+      const obj2 = {
+        str,
+        depth,
+      };
+      return getFormattedArray(obj.keyValue, depth + 1);
+    }
+    return null;
+  });
+  return result;
+}
+
+const getStylishFormat = (diff) => {
+  const result = getFormattedArray(diff);
+  console.log(result);
+  const result2 = result.map((el) => el.str);
+  return result2.join('');
+}
 export default getStylishFormat;
