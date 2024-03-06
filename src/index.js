ab59d1b0335d8866ec9998d46ae415546b3e743f
Volodia K <wtraktorman94@mail.ru>
2024-03-06 23:19:43 +0500

    fix autotests

--- src/index.js
+++ src/index.js
@@ -15,25 +15,10 @@ const genDiff = (filepath1, filepath2, format = 'stylish') => {
   const parsedObj1 = parser(file1, checkFileType(filepath1));
   const parsedObj2 = parser(file2, checkFileType(filepath2));
 
-  console.log(JSON.stringify(getDiff(parsedObj1, parsedObj2), null, 2));
+  // console.log(JSON.stringify(getDiff(parsedObj1, parsedObj2), null, 2));
   const result = getFormattedDiff(getDiff(parsedObj1, parsedObj2), format);
-  console.log(result);
+  // console.log(result);
   return result;
 };
 
 export default genDiff;
-// gendiff __fixtures__/file1.yml __fixtures__/file2.yaml
-// gendiff __fixtures__/file1.json __fixtures__/file2.json
-// gendiff __fixtures__/nested.file1.json __fixtures__/nested.file2.json
-// gendiff __fixtures__/nested.file1.json __fixtures__/nested.file2.json
-
-// gendiff -f plain file1.yml file2.json
-// gendiff -f stylish file1.yml file2.json
-
-// gendiff file1.json file2.json
-// gendiff file1.yml file2.yaml
-
-// gendiff nested.file1.json nested.file2.json
-// gendiff nested.file1.yaml nested.file2.yaml
-// gendiff -f plain nested.file1.yaml nested.file2.yaml
-// gendiff -f json nested.file1.yaml nested.file2.yaml
