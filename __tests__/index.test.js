8e7d7cdcd03346d40d95ebe278f9e997f2fbafe1
Volodia K <wtraktorman94@mail.ru>
2024-03-07 19:40:16 +0500

    delete duplicating 1

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,6 +5,7 @@ import { pathResolver } from '../src/utils.js';
 const json1 = '__fixtures__/nested.file1.json';
 const json2 = '__fixtures__/nested.file2.json';
 const yaml1 = '__fixtures__/nested.file1.yaml';
+const yaml2 = '__fixtures__/nested.file2.yml';
 
 test('nested files with with .json format and stylish flag', () => {
   const nestedResultStylish = fs.readFileSync(pathResolver('nested.result.stylish.txt'), 'utf-8');
@@ -13,7 +14,7 @@ test('nested files with with .json format and stylish flag', () => {
 
 test('nested files with with .yaml format and stylish flag', () => {
   const nestedResultStylish = fs.readFileSync(pathResolver('nested.result.stylish.txt'), 'utf-8');
-  expect(genDiff(yaml1, '__fixtures__/nested.file2.yml', 'stylish')).toEqual(nestedResultStylish);
+  expect(genDiff(yaml1, yaml2, 'stylish')).toEqual(nestedResultStylish);
 });
 
 test('nested files with with .json format and plain flag', () => {
@@ -23,7 +24,7 @@ test('nested files with with .json format and plain flag', () => {
 
 test('nested files with with .yaml format and plain flag', () => {
   const nestedResultPlain = fs.readFileSync(pathResolver('nested.result.plain.txt'), 'utf-8');
-  expect(genDiff('__fixtures__/nested.file1.yaml', '__fixtures__/nested.file2.yml', 'plain')).toEqual(nestedResultPlain);
+  expect(genDiff(yaml1, yaml2, 'plain')).toEqual(nestedResultPlain);
 });
 
 test('nested files with with .json format and json flag', () => {
@@ -33,5 +34,5 @@ test('nested files with with .json format and json flag', () => {
 
 test('nested files with with .yaml format and json flag', () => {
   const nestedResultJson = fs.readFileSync(pathResolver('nested.result.json.txt'), 'utf-8');
-  expect(genDiff('__fixtures__/nested.file1.yaml', '__fixtures__/nested.file2.yml', 'json')).toEqual(nestedResultJson);
+  expect(genDiff(yaml1, yaml2, 'json')).toEqual(nestedResultJson);
 });
